9a475ec7a7bd8c3c716677c891370eaa9f2b14b7
yago <yagocam5@gmail.com>
2024-03-25 19:16:03 -0300

    jwt

--- backend_challenge/urls.py
+++ backend_challenge/urls.py
@@ -19,12 +19,17 @@ from django.urls import path, include
 
 from rest_framework import routers
 from todolist.api import viewsets 
-
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 route = routers.DefaultRouter()
 route.register(r'users',viewsets.UserViewSet, basename="Users")
 route.register(r'tasks',viewsets.TaskViewSet, basename="Tasks")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('',include(route.urls))
 ]
--- todolist/api/viewsets.py
+++ todolist/api/viewsets.py
@@ -1,11 +1,14 @@
 from rest_framework import viewsets
 from todolist.api import serializers
 from todolist import models
+from rest_framework.permissions import IsAuthenticated
 
 class UserViewSet(viewsets.ModelViewSet):
+    permission_classes = (IsAuthenticated,)
     serializer_class = serializers.UserSerializer
     queryset = models.User.objects.all()
 
 class TaskViewSet(viewsets.ModelViewSet):
+    permission_classes = (IsAuthenticated,)
     serializer_class = serializers.TaskSerializer
     queryset = models.Task.objects.all()    
