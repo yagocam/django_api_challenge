04bcd9ce1830a8602d5fa780a26da41c15a7c0ed
yago <yagocam5@gmail.com>
2024-03-26 00:04:55 -0300

    static_url

--- backend_challenge/settings.py
+++ backend_challenge/settings.py
@@ -137,7 +137,6 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = 'static/'
 STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
