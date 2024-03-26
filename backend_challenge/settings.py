c24974c19ac5519a203aad573dc6b124f2015495
yago <yagocam5@gmail.com>
2024-03-25 23:48:50 -0300

    settings

--- backend_challenge/settings.py
+++ backend_challenge/settings.py
@@ -130,7 +130,10 @@ USE_I18N = True
 
 USE_TZ = True
 
-
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+]
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
